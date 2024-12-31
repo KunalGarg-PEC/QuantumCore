2e4c13352de158a8488a46bb1a38d097fb2072e3
KunalGarg-PEC <gargkunal369@gmail.com>
2024-12-31 18:03:51 +0530

    Initial Commit

---
+++ src/constants/index.jsx
@@ -0,0 +1,122 @@
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+
+
+
+export const navItems = [
+  { label: "Home", href: "#" },
+  { label: "Features", href: "#" },
+  { label: "WorkFlow", href: "#" },
+];
+
+
+
+export const features = [
+  {
+    icon: <BotMessageSquare />,
+    text: "Quantum Speed",
+    description:
+      "Proprietary quantum chips with unmatched processing speed.",
+  },
+  {
+    icon: <Fingerprint />,
+    text: "Turnkey Systems",
+    description:
+      "Turnkey quantum systems for businesses and research institutions.",
+  },
+  {
+    icon: <ShieldHalf />,
+    text: "Custom Solutions",
+    description:
+      "Tailored solutions for industries like finance, healthcare, and AI.",
+  },
+  {
+    icon: <BatteryCharging />,
+    text: "Developer Support",
+    description:
+      "Supportive infrastructure for quantum developers and researchers.",
+  }
+  
+];
+
+export const checklistItems = [
+  {
+    title: "Quantum Speed",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+  {
+    title: "Turnkey Systems",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+  {
+    title: "Custom Solutions",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+  {
+    title: "Developer Support",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+  },
+];
+
+export const pricingOptions = [
+  {
+    title: "Free",
+    price: "$0",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Pro",
+    price: "$10",
+    features: [
+      "Private board sharing",
+      "10 Gb Storage",
+      "Web Analytics (Advance)",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "$200",
+    features: [
+      "Private board sharing",
+      "Unlimited Storage",
+      "High Performance Network",
+      "Private Mode",
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { href: "#", text: "Getting Started" },
+  { href: "#", text: "Documentation" },
+  { href: "#", text: "Tutorials" },
+  { href: "#", text: "API Reference" },
+  { href: "#", text: "Community Forums" },
+];
+
+export const platformLinks = [
+  { href: "#", text: "Features" },
+  { href: "#", text: "Supported Devices" },
+  { href: "#", text: "System Requirements" },
+  { href: "#", text: "Downloads" },
+  { href: "#", text: "Release Notes" },
+];
+
+export const communityLinks = [
+  { href: "#", text: "Events" },
+  { href: "#", text: "Meetups" },
+  { href: "#", text: "Conferences" },
+  { href: "#", text: "Hackathons" },
+  { href: "#", text: "Jobs" },
+];
